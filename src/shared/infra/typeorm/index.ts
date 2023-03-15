8766c1f2d008e0d623554ee3903b7a60fe79bd23
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2023-03-15 17:35:41 -0300

    ajuste conexao

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,18 +1,32 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database"): Promise<Connection> => {
+//para rodar com a api no docker
+// export default async (host = "database"): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === "test"
+//         ? "localhost"
+//         : host,
+//       database: process.env.NODE_ENV === "test"
+//         ? "rentx_test"
+//         : defaultOptions.database,
+//     })
+//   );
+
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test"
-        ? "localhost"
-        : host,
       database: process.env.NODE_ENV === "test"
         ? "rentx_test"
         : defaultOptions.database,
     })
   );
+
 
 }
